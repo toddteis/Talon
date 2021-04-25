dce4d563dc5aa2434b2f4d470b06564c73422da0
Todd <toddteis@gmail.com>
2021-04-26 07:26:52 +1000

    End of stage 13.1 hide/show nav links

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
+    // [AllowAnonymous]
     public class OrdersController : BaseApiController
     {
         [HttpGet]
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -20,12 +20,23 @@ export default observer(function NavBar() {
                 <Menu.Item as={NavLink} to='/products' name='Products' />
                 <Menu.Item as={NavLink} to='/about' name='About us' />
                 <Menu.Item as={NavLink} to='/contact' name='Contact us' />
-                <Menu.Item as={NavLink} to='/resources' name='Resources' />
-                <Menu.Item as={NavLink} to='/orders' name='Orders' />
-                <Menu.Item as={NavLink} to='/errors' name='Errors' />
-                <Menu.Item>
-                    <Button as={NavLink} to='/createOrder' positive content='Create Order' />
-                </Menu.Item>
+                <Menu.Menu position='left'>
+                    {userStore.isLoggedIn ? (
+                        <>
+                        <Menu.Item as={NavLink} to='/resources' name='Resources' />
+                        <Menu.Item as={NavLink} to='/orders' name='Orders' />
+                        <Menu.Item>
+                            <Button as={NavLink} to='/createOrder' positive content='Create Order' />
+                        </Menu.Item>
+                        </>
+                    ) : (
+                        <>
+                        </>
+                    )}
+                    
+                </Menu.Menu>
+
+
                 <Menu.Item position='right'>
                     {userStore.isLoggedIn ? (
                         <Dropdown pointing='top left' text={userStore.user?.displayName}>
